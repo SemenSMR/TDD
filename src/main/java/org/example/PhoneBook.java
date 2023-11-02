bb05914be833bd6adbfcc46f93bf9fecf9bf2c87
Evgeniy <svorovali@mail.ru>
2023-11-02 09:20:51 +0300

    add implemented

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
-    public  int add(String name, int number){
-        return 0;
+    private Map<String, Integer> map = new HashMap<>();
+    private int countAdd = 0;
+
+    public int add(String name, int number) {
+
+        if (!map.containsKey(name)) {
+            map.put(name, number);
+            countAdd++;
+        } return countAdd;
     }
 }
+
