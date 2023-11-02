36d97feac12f627160183b9e8e710cde99c1551c
Evgeniy <svorovali@mail.ru>
2023-11-02 13:23:40 +0300

    refactoring

--- src/test/java/org/example/PhoneBookTest.java
+++ src/test/java/org/example/PhoneBookTest.java
@@ -16,6 +16,9 @@ class PhoneBookTest {
     @BeforeEach
     public void setUp() {
         phoneBook = new PhoneBook();
+
+        phoneBook.add("Evgenii", 123124512);
+        phoneBook.add("Dmitriy", 346787239);
     }
 
 
@@ -23,21 +26,19 @@ class PhoneBookTest {
     public void add() {
 
         int result = phoneBook.add("Evgenii", 123124512);
-        assertEquals(1, result);
-
-        result = phoneBook.add("Evgenii", 125645234);
-        assertEquals(1, result);
+        assertEquals(2, result);
 
         result = phoneBook.add("Dmitriy", 346787239);
         assertEquals(2, result);
+        result = phoneBook.add("Pasha", 568945892);
+        assertEquals(3, result);
+
 
     }
 
     @Test
     public void findByNumber() {
 
-        phoneBook.add("Evgenii", 123124512);
-        phoneBook.add("Dmitriy", 346787239);
 
         String name = phoneBook.findByNumber(123124512);
         assertEquals("Evgenii", name);
@@ -49,20 +50,17 @@ class PhoneBookTest {
 
     @Test
     public void findByName() {
-        phoneBook.add("Evgenii", 123124512);
-        phoneBook.add("Dmitriy", 346787239);
 
         int number = phoneBook.findByName("Dmitriy");
         assertEquals(346787239, number);
 
         int error = phoneBook.findByName("Mihail");
-        assertEquals(-1,error);
+        assertEquals(-1, error);
     }
+
     @Test
     public void printAllNames() {
 
-        phoneBook.add("Evgenii", 123124512);
-        phoneBook.add("Dmitriy", 346787239);
         phoneBook.add("Alina", 1254734223);
         phoneBook.add("Dasha", 964515521);
 
@@ -70,7 +68,7 @@ class PhoneBookTest {
 
         List<String> expected = Arrays.asList("Alina", "Dasha", "Dmitriy", "Evgenii");
 
-        assertEquals(expected,result);
+        assertEquals(expected, result);
 
     }
 }
