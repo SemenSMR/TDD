8c23dd08aefd19da7cdbbd06e521a3e46dbb12ed
Evgeniy <svorovali@mail.ru>
2023-11-02 09:03:07 +0300

    Stub and tests for add

--- src/test/java/org/example/PhoneBookTest.java
+++ src/test/java/org/example/PhoneBookTest.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PhoneBookTest {
 
+    @Test
+    public void add() {
+        PhoneBook phoneBook = new PhoneBook();
+
+        int result = phoneBook.add("Evgenii", 123124512);
+        assertEquals(1,result);
+
+        result = phoneBook.add("Evgenii" , 125645234);
+        assertEquals(1,result);
+
+        result = phoneBook.add("Dmitriy", 346787239);
+        assertEquals(2,result);
+
+
+    }
 }
